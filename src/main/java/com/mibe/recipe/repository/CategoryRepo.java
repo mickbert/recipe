6e550f4a9eb49a095df7a0c879b54d0e2d167503
Mick <MickBert@posteo.net>
2020-05-25 15:29:10 +0200

    aggiunti metodi di ricerca ai repository per categorie e ricette

--- src/main/java/com/mibe/recipe/repository/CategoryRepo.java
+++ src/main/java/com/mibe/recipe/repository/CategoryRepo.java
@@ -3,5 +3,8 @@ package com.mibe.recipe.repository;
 import com.mibe.recipe.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepo extends CrudRepository<Category, Long> {
+    Optional<Category> findByDescription(String aDesc);
 }
--- src/main/java/com/mibe/recipe/repository/RecipeRepo.java
+++ src/main/java/com/mibe/recipe/repository/RecipeRepo.java
@@ -3,5 +3,8 @@ package com.mibe.recipe.repository;
 import com.mibe.recipe.domain.Recipe;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface RecipeRepo extends CrudRepository<Recipe,Long> {
+    Optional<Recipe> findByDescription(String aDesc);
 }
