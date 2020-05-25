e509d60a9469e02acfeee3ea13280822e548adcc
Mick <MickBert@posteo.net>
2020-05-25 15:34:16 +0200

    Inizializzazione tabelle categorie e unità di misura

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,9 +2,9 @@ insert into category (name,description) values('Italian','Italian food');
 insert into category (name,description) values('French','French recipes');
 insert into category (name,description) values('Italian','Italian dishes');
 insert into category (name,description) values('Chinese','Chinese cooking');
-insert into category (name,description) values('Japanish','Sushi');
+insert into category (name,description) values('Japanese','Sushi');
 
 insert into unit_of_measure (name,code) values('grams','g');
 insert into unit_of_measure (name,code) values('deciliter','dl');
 insert into unit_of_measure (name,code) values('tea spoon','t-sp');
-insert into unit_of_measure (name,code) values('soupe spoon','sp');
+insert into unit_of_measure (name,code) values('soup spoon','sp');
